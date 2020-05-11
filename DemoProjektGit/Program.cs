978324d4a78d9fa3835dcdfcc5592cac90388b8a
Glenn Jönsson <glennj83@hotmail.com>
2020-05-11 10:40:14 +0200

    har ändrat en consoleline

--- DemoProjektGit/Program.cs
+++ DemoProjektGit/Program.cs
@@ -6,7 +6,7 @@ namespace DemoProjektGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello my team");
         }
     }
 }
